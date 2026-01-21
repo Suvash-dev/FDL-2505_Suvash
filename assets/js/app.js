54a28cede02e47d5d8cf607e5f3457d6b3afbc86
Suvash-dev <ctgsuvash@gmail.com>
2026-01-21 18:06:00 +0600

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,37 @@
 $('.srcInMob').click(function(){
     $('.mblsrcarea').toggleClass('d-none');
 }) 
+
+/*banner Slider*/
+
+
+$(document).ready(function(){
+  $('.SliderBase').slick({
+    dots: true,          
+    dotsClass:'my-dots',
+    infinite: true,
+    speed: 500,
+    fade: true,          
+    cssEase: 'linear',
+    arrows: false,       
+    autoplay: true,
+    autoplaySpeed: 2000    
+  });
+});
+
+
+/*product Slider*/
+
+$(document).ready(function(){
+  $('.featuredSlideBase1').slick({
+    dots: true,          
+    dotsClass:'my-dots',
+    infinite: true,
+    speed: 500,
+    fade: true,          
+    cssEase: 'linear',
+    arrows: false,       
+    autoplay: true,
+    autoplaySpeed: 2000    
+  });
+});
