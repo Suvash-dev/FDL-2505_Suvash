51272c34b10e4a9e2d753a5c6d75bba6d910e99d
Suvash-dev <ctgsuvash@gmail.com>
2026-01-28 19:23:53 +0600

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,36 +2,59 @@ $('.srcInMob').click(function(){
     $('.mblsrcarea').toggleClass('d-none');
 }) 
 
-/*banner Slider*/
-
 
 $(document).ready(function(){
   $('.SliderBase').slick({
-    dots: true,          
+    dots: true,          // Shows the pagination dots
     dotsClass:'my-dots',
     infinite: true,
     speed: 500,
-    fade: true,          
+    fade: true,          // Smooth fade transition
     cssEase: 'linear',
-    arrows: false,       
+    arrows: false,       // Hide side arrows if you only want dots
     autoplay: true,
     autoplaySpeed: 2000    
   });
 });
 
 
-/*product Slider*/
-
-$(document).ready(function(){
-  $('.featuredSlideBase1').slick({
-    dots: true,          
-    dotsClass:'my-dots',
-    infinite: true,
-    speed: 500,
-    fade: true,          
-    cssEase: 'linear',
-    arrows: false,       
-    autoplay: true,
-    autoplaySpeed: 2000    
+  $(document).ready(function() {
+    $('.category-slider').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: false,
+      dotsClass:'my-dots',
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 5
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 2
+          }
+        }
+      ]
+    });
   });
-});
+
